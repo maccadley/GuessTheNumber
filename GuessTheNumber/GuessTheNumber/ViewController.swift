04ff7202cdbfbf07a00ab4af8870244b77365c4e
maccadley <>
2020-01-21 20:06:22 +0300

    Started with StoryBoard

---
+++ GuessTheNumber/GuessTheNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessTheNumber
+//
+//  Created by Admin on 21.01.2020.
+//  Copyright © 2020 MaximMasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
